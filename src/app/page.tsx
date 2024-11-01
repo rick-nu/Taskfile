c9e1f8bf5188665002b61a3308ec4b52969596d5
Rick van der Staaij <id@rick.nu>
2024-11-01 20:39:29 +0100

    Add page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import Settings from '@/components/Generator';
+import {Metadata} from "next";
+
+export const generateMetadata = (): Metadata => ({
+	title: 'Taskfile generator',
+	description: 'Quickly kick start your project by moving all your development commands to one easy to understand and maintain place.',
+});
 
 export default function Home() {
 	return (
