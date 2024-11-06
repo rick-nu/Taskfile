11bcd8be652847ff167c5ea396cec4e27e72e39f
Rick van der Staaij <id@rick.nu>
2024-11-06 22:19:14 +0100

    Add typescript types check to linting

--- dev/linting/lint-staged.config.mjs
+++ dev/linting/lint-staged.config.mjs
@@ -1,17 +1,23 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+const prettier =
+	'prettier --config dev/linting/.prettierrc.json --ignore-path dev/linting/.prettierignore --write --list-different';
+
 const config = {
 	// ==============================
 	// Frontend specific
 	// ==============================
 	'*.{ts,tsx}': [
 		// tsc runs for all files instead of only the edited ones
-		() => './node_modules/.bin/tsc --noEmit --project . --pretty',
+		() => 'tsc --noEmit --project . --pretty',
+		'eslint --config dev/linting/eslint.config.mjs',
+		prettier,
 	],
 
 	// ==============================
 	// Global files
 	// ==============================
-	'*.{json,css,md,html}': ['prettier --write --ignore-path dev/.prettierignore'],
+	'*.{json,css,md,html}': [prettier],
 };
 
 export default config;
