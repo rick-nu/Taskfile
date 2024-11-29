0951aa2120a84f03b1dc105dafafbed3c219e111
Rick van der Staaij <id@rick.nu>
2024-11-29 16:05:19 +0100

    temporary info content

--- src/app/information/page.tsx
+++ src/app/information/page.tsx
@@ -11,9 +11,77 @@ export default function Page() {
 	return (
 		<Content>
 			<h1>Empty</h1>
-			<p>Scratch the furniture brown cats with pink ears i cry and cry and cry unless you pet me, and then maybe i cry just for fun growl at dogs in my sleep. Pet me pet me pet me pet me, bite, scratch, why are you petting me. Be a nyan cat, feel great about it, be annoying 24/7 poop rainbows in litter box all day if it smells like fish eat as much as you wish bite off human's toes or munch on tasty moths so purr purr purr until owner pets why owner not pet me hiss scratch meow. Dont wait for the storm to pass, dance in the rain meow to be let out, but push your water glass on the floor scratch nyan fluffness ahh cucumber!. Sleep small kitty warm kitty little balls of fur or i like to spend my days sleeping and eating fishes that my human fished for me we live on a luxurious yacht, sailing proudly under the sun, i like to walk on the deck, watching the horizon, dreaming of a good bowl of milk. Plop down in the middle where everybody walks pet me pet me pet me pet me, bite, scratch, why are you petting me for my water bowl is clean and freshly replenished, so i'll drink from the toilet nya nya nyan and miaow then turn around and show you my bum. Chase imaginary bugs enslave the hooman or spread kitty litter all over house yet eat an easter feather as if it were a bird then burp victoriously, but tender plan steps for world domination cuddle no cuddle cuddle love scratch scratch. Hiding behind the couch until lured out by a feathery toy ask to be pet then attack owners hand but burrow under covers, but as lick i the shoes, refuse to come home when humans are going to bed; stay out all night then yowl like i am dying at 4am my left donut is missing, as is my right. Terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not sorry leave buried treasure in the sandbox for the toddlers. </p>
-			<p>Drink from the toilet climb into cupboard and lick the salt off rice cakes but no, you can't close the door, i haven't decided whether or not i wanna go out. One of these days i'm going to get that red dot, just you wait and see do not try to mix old food with new one to fool me!, but stares at human while pushing stuff off a table check cat door for ambush 10 times before coming in yet whatever enslave the hooman make it to the carpet before i vomit mmmmmm. Fall over dead (not really but gets sypathy) eat a plant, kill a hand, or love me! funny little cat chirrup noise shaking upright tail when standing next to you leave fur on owners clothes. Push your water glass on the floor am in trouble, roll over, too cute for human to get mad. Refuse to leave cardboard box to pet a cat, rub its belly, endure blood and agony, quietly weep, keep rubbing belly intently stare at the same spot, so meowsiers so walk on keyboard jump on fridge sees bird in air, breaks into cage and attacks creature. Man running from cops stops to pet cats, goes to jail enslave the hooman so walk on keyboard . Cats are cute plop down in the middle where everybody walks. Bring your owner a dead bird sleep, cat is love, cat is life trip on catnip or meowing non stop for food. Dream about hunting birds touch water with paw then recoil in horror behind the couch. Sleep over your phone and make cute snoring noises ooooh feather moving feather! cats are cute always hungry so scratch leg; meow for can opener to feed me meow meow mama. Purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table. Roll over and sun my belly cat sit like bread stuff and things but run outside as soon as door open good now the other hand, too. Stare at ceiling i do no work yet get food, shelter, and lots of stuff just like man who lives with us and stare at ceiling light lick the other cats for stinky cat so scratch my tummy actually i hate you now fight me. Meowsiers scoot butt on the rug human is behind a closed door, emergency! abandoned! meeooowwww!!! purr when give birth playing with balls of wool. Just going to dip my paw in your coffee and do a taste test - oh never mind i forgot i don't like coffee - you can have that back now destroy the blinds, the fat cat sat on the mat bat away with paws or eat plants, meow, and throw up because i ate plants or run off table persian cat jump eat fish yet chill on the couch table. The fat cat sat on the mat bat away with paws chase imaginary bugs chase red laser dot or i heard this rumor where the humans are our owners, pfft, what do they know?! this cat happen now, it was too purr-fect!!!. Carefully drink from water glass and then spill it everywhere and proceed to lick the puddle scratch at the door then walk away yet cats are a queer kind of folk. Ptracy russian blue catty ipsum i like frogs and 0 gravity. Proudly present butt to human leave fur on owners clothes stare at the wall, play with food and get confused by dust and i will be pet i will be pet and then i will hiss so reward the chosen human with a slow blink for relentlessly pursues moth. Poop on the floor, break a planter, sprint, eat own hair, vomit hair, hiss, chirp at birds, eat a squirrel, hide from fireworks, lick toe beans, attack christmas tree cough furball for disappear for four days and return home with an expensive injury; bite the vet so paw at beetle and eat it before it gets away. Only use one corner of the litter box plays league of legends try to jump onto window and fall while scratching at wall look at dog hiiiiiisssss. Meow meow you are my owner so here is a dead bird hide head under blanket so no one can see. Lick left leg for ninety minutes, still dirty purr purr purr until owner pets why owner not pet me hiss scratch meow and commence midnight zoomies refuse to come home when humans are going to bed; stay out all night then yowl like i am dying at 4am and nyan fluffness ahh cucumber! or stare at ceiling. Pee on walls it smells like breakfast meow in empty rooms climb leg hide when guests come over, so sniff other cat's butt and hang jaw half open thereafter or i can haz yet attack the child. Dead stare with ears cocked sweet beast, for push your water glass on the floor. </p>
-			<p>Love to play with owner's hair tie give me attention or face the wrath of my claws meowing non stop for food poop on couch side-eyes your "jerk" other hand while being petted , grab pompom in mouth and put in water dish. Attack feet. Look at dog hiiiiiisssss sit on the laptop and i shall purr myself to sleep or touch my tail, i shred your hand purrrr meow meow meow we are 3 small kittens sleeping most of our time, we are around 15 weeks old i think, i donâ€™t know i canâ€™t count. Roll on the floor purring your whiskers off. Cry louder at reflection reaches under door into adjacent room you are a captive audience while sitting on the toilet, pet me and lay on arms while you're using the keyboard sleep over your phone and make cute snoring noises hit you unexpectedly but lick yarn hanging out of own butt. Allways wanting food play time russian blue lick sellotape and i like fish. </p>
+			<p>
+				Scratch the furniture brown cats with pink ears i cry and cry and cry unless you pet me, and then maybe i cry
+				just for fun growl at dogs in my sleep. Pet me pet me pet me pet me, bite, scratch, why are you petting me. Be a
+				nyan cat, feel great about it, be annoying 24/7 poop rainbows in litter box all day if it smells like fish eat
+				as much as you wish bite off human's toes or munch on tasty moths so purr purr purr until owner pets why owner
+				not pet me hiss scratch meow. Dont wait for the storm to pass, dance in the rain meow to be let out, but push
+				your water glass on the floor scratch nyan fluffness ahh cucumber!. Sleep small kitty warm kitty little balls of
+				fur or i like to spend my days sleeping and eating fishes that my human fished for me we live on a luxurious
+				yacht, sailing proudly under the sun, i like to walk on the deck, watching the horizon, dreaming of a good bowl
+				of milk. Plop down in the middle where everybody walks pet me pet me pet me pet me, bite, scratch, why are you
+				petting me for my water bowl is clean and freshly replenished, so i'll drink from the toilet nya nya nyan and
+				miaow then turn around and show you my bum. Chase imaginary bugs enslave the hooman or spread kitty litter all
+				over house yet eat an easter feather as if it were a bird then burp victoriously, but tender plan steps for
+				world domination cuddle no cuddle cuddle love scratch scratch. Hiding behind the couch until lured out by a
+				feathery toy ask to be pet then attack owners hand but burrow under covers, but as lick i the shoes, refuse to
+				come home when humans are going to bed; stay out all night then yowl like i am dying at 4am my left donut is
+				missing, as is my right. Terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not sorry leave
+				buried treasure in the sandbox for the toddlers.
+			</p>
+			<p>
+				Drink from the toilet climb into cupboard and lick the salt off rice cakes but no, you can't close the door, i
+				haven't decided whether or not i wanna go out. One of these days i'm going to get that red dot, just you wait
+				and see do not try to mix old food with new one to fool me!, but stares at human while pushing stuff off a table
+				check cat door for ambush 10 times before coming in yet whatever enslave the hooman make it to the carpet before
+				i vomit mmmmmm. Fall over dead (not really but gets sypathy) eat a plant, kill a hand, or love me! funny little
+				cat chirrup noise shaking upright tail when standing next to you leave fur on owners clothes. Push your water
+				glass on the floor am in trouble, roll over, too cute for human to get mad. Refuse to leave cardboard box to pet
+				a cat, rub its belly, endure blood and agony, quietly weep, keep rubbing belly intently stare at the same spot,
+				so meowsiers so walk on keyboard jump on fridge sees bird in air, breaks into cage and attacks creature. Man
+				running from cops stops to pet cats, goes to jail enslave the hooman so walk on keyboard . Cats are cute plop
+				down in the middle where everybody walks. Bring your owner a dead bird sleep, cat is love, cat is life trip on
+				catnip or meowing non stop for food. Dream about hunting birds touch water with paw then recoil in horror behind
+				the couch. Sleep over your phone and make cute snoring noises ooooh feather moving feather! cats are cute always
+				hungry so scratch leg; meow for can opener to feed me meow meow mama. Purr as loud as possible, be the most
+				annoying cat that you can, and, knock everything off the table. Roll over and sun my belly cat sit like bread
+				stuff and things but run outside as soon as door open good now the other hand, too. Stare at ceiling i do no
+				work yet get food, shelter, and lots of stuff just like man who lives with us and stare at ceiling light lick
+				the other cats for stinky cat so scratch my tummy actually i hate you now fight me. Meowsiers scoot butt on the
+				rug human is behind a closed door, emergency! abandoned! meeooowwww!!! purr when give birth playing with balls
+				of wool. Just going to dip my paw in your coffee and do a taste test - oh never mind i forgot i don't like
+				coffee - you can have that back now destroy the blinds, the fat cat sat on the mat bat away with paws or eat
+				plants, meow, and throw up because i ate plants or run off table persian cat jump eat fish yet chill on the
+				couch table. The fat cat sat on the mat bat away with paws chase imaginary bugs chase red laser dot or i heard
+				this rumor where the humans are our owners, pfft, what do they know?! this cat happen now, it was too
+				purr-fect!!!. Carefully drink from water glass and then spill it everywhere and proceed to lick the puddle
+				scratch at the door then walk away yet cats are a queer kind of folk. Ptracy russian blue catty ipsum i like
+				frogs and 0 gravity. Proudly present butt to human leave fur on owners clothes stare at the wall, play with food
+				and get confused by dust and i will be pet i will be pet and then i will hiss so reward the chosen human with a
+				slow blink for relentlessly pursues moth. Poop on the floor, break a planter, sprint, eat own hair, vomit hair,
+				hiss, chirp at birds, eat a squirrel, hide from fireworks, lick toe beans, attack christmas tree cough furball
+				for disappear for four days and return home with an expensive injury; bite the vet so paw at beetle and eat it
+				before it gets away. Only use one corner of the litter box plays league of legends try to jump onto window and
+				fall while scratching at wall look at dog hiiiiiisssss. Meow meow you are my owner so here is a dead bird hide
+				head under blanket so no one can see. Lick left leg for ninety minutes, still dirty purr purr purr until owner
+				pets why owner not pet me hiss scratch meow and commence midnight zoomies refuse to come home when humans are
+				going to bed; stay out all night then yowl like i am dying at 4am and nyan fluffness ahh cucumber! or stare at
+				ceiling. Pee on walls it smells like breakfast meow in empty rooms climb leg hide when guests come over, so
+				sniff other cat's butt and hang jaw half open thereafter or i can haz yet attack the child. Dead stare with ears
+				cocked sweet beast, for push your water glass on the floor.
+			</p>
+			<p>
+				Love to play with owner's hair tie give me attention or face the wrath of my claws meowing non stop for food
+				poop on couch side-eyes your "jerk" other hand while being petted , grab pompom in mouth and put in water dish.
+				Attack feet. Look at dog hiiiiiisssss sit on the laptop and i shall purr myself to sleep or touch my tail, i
+				shred your hand purrrr meow meow meow we are 3 small kittens sleeping most of our time, we are around 15 weeks
+				old i think, i donâ€™t know i canâ€™t count. Roll on the floor purring your whiskers off. Cry louder at
+				reflection reaches under door into adjacent room you are a captive audience while sitting on the toilet, pet me
+				and lay on arms while you're using the keyboard sleep over your phone and make cute snoring noises hit you
+				unexpectedly but lick yarn hanging out of own butt. Allways wanting food play time russian blue lick sellotape
+				and i like fish.
+			</p>
 		</Content>
 	);
 }
