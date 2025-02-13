58424c54945158e9491eb3a4d561ae01c8d39b78
Rick van der Staaij <id@rick.nu>
2025-02-13 11:28:04 +0100

    Give positive feedback when utilities pass

--- src/components/Generator/GeneredTaskfile/addons/appUtilities/app-utilities.sh
+++ src/components/Generator/GeneredTaskfile/addons/appUtilities/app-utilities.sh
@@ -1,6 +1,8 @@
-function app:ensure {
-    if [ ! $(which "$1") ]; then
+function app:ensure { # Abort if the desired program is not installed
+	if [ ! $(which "$1") ]; then
 		echo "Missing required application ${RED}$1${RESET}. Install it before tying again."
 		exit 1
-    fi
+	else
+		echo "Application ${GREEN}$1${RESET} is installed.";
+	fi
 }
--- src/components/Generator/GeneredTaskfile/addons/fileUtilities/file-utilities.sh
+++ src/components/Generator/GeneredTaskfile/addons/fileUtilities/file-utilities.sh
@@ -2,14 +2,16 @@ function file:ensure { # Abort if the desired file is not found
 	if [ ! -f $1 ]; then
 		echo -e "Missing required file ${RED}$1${RESET}, make sure it is created."
 		exit 1
+	else
+		echo "File ${GREEN}$1${RESET} is present.";
 	fi
 }
 
-function file:ensure-copy { # file:ensure-copy $COPY_TARGET $COPY_SOURCE
-    if [ ! -f $1 ]; then
+function file:ensure-copy { # file:ensure-copy $COPY_DESTINATION $SOURCE
+	if [ ! -f $1 ]; then
 		cp $2 $1;
 		echo -e "Created copy of ${YELLOW}$2${RESET} to create required file ${GREEN}$1${RESET}.";
 	else
 		echo "File ${GREEN}$1${RESET} is present.";
-    fi
+	fi
 }
